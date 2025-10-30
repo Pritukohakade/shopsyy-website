39a025ebb019db559cca8c0642aa09508c3c19fa
Pritukohakade <pritakohakade@gmail.com>
2025-10-30 10:02:08 +0530

    Fix import path for ShopContext

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { ShopContext } from '../Context/ShopContext'
+import { ShopContext } from '../context/ShopContext'
 import { useParams } from 'react-router-dom';
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
 import Bredcrums from '../components/Bredcrums/Bredcrums';
